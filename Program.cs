22712e1ac33ca1f44e7055b16916ceef95c4f0da
Edward Almond <eddie.almond@qtac.co.uk>
2019-01-09 17:11:13 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                WriteLine("Please provide exactly 1 argument - the text to split.");
+                return;
+            }
+
+            var messageInput = args[0];
+
+            var configuration = new SplitterConfiguration();
+
+            var splitter = new Splitter(configuration);
+
+            var tweets = splitter.Split(messageInput);
+
+            for (int i = 0; i < tweets.Count(); i++)
+            {
+                var tweet = tweets.ElementAt(i);
+                WriteLine($"Printing Tweet {i + 1} of {tweets.Count()}");
+                WriteLine($"\"{tweet.Message}\"");
+                WriteLine();
+            }
+        }
+    }
+}
---
+++ Splitter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    /// <summary>
+    /// Splits a single long message into a series of Tweets 
+    /// </summary>
+    public class Splitter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Splitter"/> class.
+        /// </summary>
+        /// <param name="splitterConfiguration">The splitter configuration.</param>
+        /// <exception cref="NotImplementedException"></exception>
+        public Splitter(SplitterConfiguration splitterConfiguration)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Splits the specified message into a series of tweets.
+        /// </summary>
+        /// <param name="message">The message to be split.</param>
+        /// <returns>A series of tweets to be posted to Twitter, in the order in which they should be posted.</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public IEnumerable<ITweet> Split(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
