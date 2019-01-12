88d13da96d8211e69e899955d4806a4372aa4ccb
Edward Almond <eddie.almond@qtac.co.uk>
2019-01-12 10:41:17 +0000

    Message is split appropriately

--- Program.cs
+++ Program.cs
@@ -7,15 +7,20 @@ namespace Core
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            if (args.Length != 1)
-            {
-                WriteLine("Please provide exactly 1 argument - the text to split.");
-                return;
-            }
+            //if (args.Length != 1)
+            //{
+            //    WriteLine("Please provide exactly 1 argument - the text to split.");
+            //    return;
+            //}
 
-            var messageInput = args[0];
+            //var messageInput = args[0];
+            var messageInput = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut iaculis sollicitudin porta. Aenean at semper augue, " +
+                "sed hendrerit augue. Sed eu tincidunt nunc, sed vestibulum ipsum. Vestibulum aliquet ex felis, vitae cursus odio vulputate nec. " +
+                "Nulla vitae dictum ipsum, sit amet pharetra lorem. Interdum et malesuada fames ac ante ipsum primis in faucibus.";
 
             var configuration = new SplitterConfiguration();
 
@@ -27,7 +32,7 @@ namespace Core
             {
                 var tweet = tweets.ElementAt(i);
                 WriteLine($"Printing Tweet {i + 1} of {tweets.Count()}");
-                WriteLine($"\"{tweet.Message}\"");
+                WriteLine($"\"{tweet}\"");
                 WriteLine();
             }
         }
--- Splitter.cs
+++ Splitter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Core
 {
@@ -9,6 +11,7 @@ namespace Core
     /// </summary>
     public class Splitter
     {
+        private readonly SplitterConfiguration _splitterConfiguration;
         /// <summary>
         /// Initializes a new instance of the <see cref="Splitter"/> class.
         /// </summary>
@@ -16,7 +19,7 @@ namespace Core
         /// <exception cref="NotImplementedException"></exception>
         public Splitter(SplitterConfiguration splitterConfiguration)
         {
-            throw new NotImplementedException();
+            _splitterConfiguration = splitterConfiguration;   
         }
 
         /// <summary>
@@ -25,9 +28,48 @@ namespace Core
         /// <param name="message">The message to be split.</param>
         /// <returns>A series of tweets to be posted to Twitter, in the order in which they should be posted.</returns>
         /// <exception cref="NotImplementedException"></exception>
-        public IEnumerable<ITweet> Split(string message)
+        //public IEnumerable<ITweet> Split(string message)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public IEnumerable<string> Split(string message)
         {
-            throw new NotImplementedException();
-        }
+            var tweets = new List<string>();
+            var sb = new StringBuilder();
+
+            List<string> words = message.Split(' ').ToList();
+            List<string> adjustedWords = words;
+           
+            for(int i = 0; i < adjustedWords.Count;)
+            {
+                sb.Append(adjustedWords[i] + " ");
+
+                if (sb.Length > _splitterConfiguration.MaximumTweetLength)
+                {
+                    var sbShortened = sb.ToString().Substring(0, sb.Length - (adjustedWords[i].Length + 1));
+
+                    tweets.Add(sbShortened);
+
+                    sb.Length = 0;
+                }
+                else if (adjustedWords.Count == 1)
+                {
+                    tweets.Add(sb.ToString());
+
+                    adjustedWords.Remove(adjustedWords[i]);
+                }
+                else
+                {
+                    adjustedWords.Remove(adjustedWords[i]);
+
+                    i = 0;
+                }
+            }
+
+            return tweets;
+        }  
+        
+        
     }
 }
